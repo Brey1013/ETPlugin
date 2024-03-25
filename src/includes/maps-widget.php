d5197d47a4e79ffeb4d748a3eec575b0268dea91
Brey1013 <brey1013@gmail.com>
2024-03-25 20:51:43 +0200

    Brey 250324

--- src/includes/maps-widget.php
+++ src/includes/maps-widget.php
@@ -4,6 +4,7 @@ if ($full_address) { ?>
       <section class="site-content-block classima-single-map">
             <div class="main-title-block">
                   <h3 class="main-title">Location</h3>
+                  <hr class="et-styled-hr">
             </div>
             <div class="main-content">
                   <iframe src="https://www.google.com/maps?q=<?php echo $full_address; ?>&output=embed" width="200"
