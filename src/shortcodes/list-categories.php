23ac848eee41cc30663ba31deb3c4fe03f9edc27
Marco Cannon <marcodfonseca@gmail.com>
2024-03-27 04:32:49 +0200

    Neatened code

--- src/shortcodes/list-categories.php
+++ src/shortcodes/list-categories.php
@@ -1,26 +1,16 @@
 <?php
 
-
-
 if (!defined('ABSPATH')) {
-
     exit; // Exit if accessed directly.
-
 }
 
-
-
 require $PLUGIN_ABSPATH . '/constants/index.php';
-
-
 
 function et_list_categories()
 {
 
-
     $taxonomy = 'ad_category';
     $terms = get_terms($taxonomy); // Get all terms of a taxonomy
-
     if ($terms && !is_wp_error($terms)):
         ?>
         <ul>
@@ -33,13 +23,7 @@ function et_list_categories()
     <?php endif; ?>
 
 
-
-
     <?php
-
 }
 
-
-
 add_shortcode('et-list-categorieset-list-categories', 'et_list_categories');
-
